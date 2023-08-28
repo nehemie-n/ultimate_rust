12ac7ed7e1e4ee5fe0339210616ee19dd0bbaff4
nehemie-n <nehemie054@gmail.com>
2023-08-28 20:29:44 +0200

    feat: mutable reference numbers

--- src/string_referencing.rs
+++ src/string_referencing.rs
@@ -1,8 +1,19 @@
+fn get_me_i32(num: &mut i32) {
+    *num = 54;
+}
+
 fn get_me_string(s: &str) {
     println!("Immutable referencing {s}")
 }
 
 pub fn simulate() {
-    let s = "Hello There";
-    get_me_string(s)
+    // Mutable borrowing
+    let mut number: i32 = 32;
+    println!("Before mutable borrowing {number}");
+    get_me_i32(&mut number);
+    println!("After mutable borrowing {number}");
+
+    // &str
+    let smut = "Hello There";
+    get_me_string(smut)
 }
